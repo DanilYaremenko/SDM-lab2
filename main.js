cfb3e1b266191a0b6100e7bb47112abb12ee56ee
DaniloYaremenko <danilyaremenko@gmail.com>
2023-03-10 02:21:35 +0200

    feat: add basic structure with Node and RingList classes

---
+++ main.js
@@ -0,0 +1,62 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class RingList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    size() {
+
+    }
+
+    append() {
+
+    }
+
+    insert() {
+
+    }
+
+    delete() {
+
+    }
+
+    deleteAll() {
+
+    }
+
+    get() {
+
+    }
+
+    clone() {
+
+    }
+
+    reverse() {
+
+    }
+
+    findFirst() {
+
+    }
+
+    findLast() {
+
+    }
+
+    clear() {
+
+    }
+
+    extend() {
+
+    }
+}
